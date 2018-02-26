fff30897fa7e05e7c74054075950843028740b19
Max Hamilton <mhamilton@vassar.edu>
2018-02-26 12:22:03 -0500

    Implemented ModelDuck

---
+++ ModelDuck.java
@@ -0,0 +1,23 @@
+public class ModelDuck extends Duck{ 
+	private String mode = "";
+    public static void main(String[] args){
+	
+    }
+    /**
+    * Constructor. Creates a new duck with name equal to name.
+    * @param name A string representing the name of this new duck.
+    */
+    public ModelDuck(String name){
+	this.name = name;
+	this.cname = "ModelDuck";
+    }
+    public void setFlyMode(String mode){
+    this.mode = mode;
+    }
+    public void fly(){
+        System.out.println("I am "
+			   +name+
+			  " of the "+cname+" and I can fly " +mode);
+    }
+
+}
