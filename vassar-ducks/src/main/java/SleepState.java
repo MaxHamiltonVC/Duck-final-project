6139bb6faae92d57c0970fd4ccb45bc3d579b19a
Max Hamilton <mhamilton@vassar.edu>
2018-05-13 15:31:25 -0400

    Added user input interpret behavior for SleepState

--- vassar-ducks/src/main/java/SleepState.java
+++ vassar-ducks/src/main/java/SleepState.java
@@ -1,6 +1,7 @@
 /**
  * Created by mhamilton on 5/8/18.
  */
+import java.util.Scanner;
 public class SleepState implements ProgramState {
     Duck programDuck;
     inputInterpreter interpreter = inputInterpreter.getInstance();
@@ -9,7 +10,58 @@ public class SleepState implements ProgramState {
     public SleepState(Duck duck){
         programDuck = duck;
     }
-    public void interpretUserInput(String userInput){}
+    public void interpretUserInput(String userInput){
+        enumUserAction processedInput = interpreter.interpret(userInput);
+        Scanner inputGatherer = new Scanner(System.in);
+        switch(processedInput){
+            case EDUCATE:
+                educate();
+                break;
+            case SLEEP:
+                System.out.println("How long would you like to sleep for?");
+                boolean improperInput = true;
+                int sleepInput = 0;
+                while(improperInput){
+                    try{
+                        sleepInput = inputGatherer.nextInt();
+                        improperInput = false;
+                    }
+                    catch(Exception e){
+                        System.out.println("Improper input--please type in an integer value.");
+                    }
+                }
+                sleep(sleepInput);
+                break;
+            case SCOLD:
+                scold();
+                break;
+            case CLEAN:
+                clean();
+                break;
+            case PLAY:
+                play(0);
+                break;
+            case MAIN:
+                nextState = enumState.MAIN;
+                break;
+            case FEED:
+                feed(0);
+                break;
+            case EXIT:
+                exit();
+                break;
+            case RESTART:
+                System.out.println("Are you sure you want to restart the duck app with a whole new duck?"
+                + programDuck.getName()+" will be lost forever!");
+                if(interpreter.interpret(inputGatherer.next())==enumUserAction.YES){
+                    System.out.println("Restarting the duck app! Say goodbye to "+programDuck.getName()+"...");
+                    restart();
+                }
+                break;
+            default:
+
+        }
+    }
     public void takeUserInput(){}
 
     public void feed(int foodAmount){}
