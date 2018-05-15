9a84fd467930d257548072eed5518d3dd38cac82
Max Hamilton <mhamilton@vassar.edu>
2018-05-14 21:38:30 -0400

    Added some documentation

--- vassar-ducks/src/main/java/ProgramState.java
+++ vassar-ducks/src/main/java/ProgramState.java
@@ -17,8 +17,19 @@ public interface ProgramState {
     public void setType(String duckType); //change to ENUM type?
 
     //Post-game-end states:
+    /**
+     * Exits the duck app. This is done by setting the next state to EXIT, which by default has the behavior
+     * of ending the loop in DuckApp.
+     * @return void
+     */
     public void exit();
     public void restart();
+
+    /**
+     * outputs the next state, which will either be the same as the current (if doing something over again),
+     * or a new one, representing a transition to a different action.
+     * @return enumState the next state of the program, as defined by enumState's guidelines
+     */
     public enumState getNextState();
     public void checkForDeath();
 }
